559439eed00b844bcb137bb86849eb5faa552f20
ben-ruhlig <benjamin.ruhlig@gmail.com>
2024-02-23 21:23:03 +0000

    Refine website outline and panels

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,66 +7,119 @@
 </script>
 
 <style>
-    /* Adjust styles as needed */
-    
-    header {
-        background-color: var(--color-pallete-dark-blue);
+    #outside {
+        display:flex;
+        flex-direction: column;
+        height: 100%;
+    }
+    header, footer {
+        background-color: black;
         display: flex;
         flex-direction: row;
         justify-content: space-evenly;
+    }
+    footer {
+        background-color: lightgrey;
+    }
+    header a, footer a {
+        text-decoration: none;
+        margin: 0 15px;
+        color: white;
+    }
+    footer a {
+        color: black;
     }
     nav {
         display: flex;
         justify-content: space-between;
         align-items: center;
-        padding: 10px 20px;
-        width: 1080px;
+        padding: 10px 40px;
+        width: var(--page-content-width);
     }
-    a {
-        text-decoration: none;
-        margin: 0 10px;
-        color: var(--color-pallete-very-light-tan);
+    #favicon, #copyright {
+        margin-left: 0px;
     }
-    .right{
+    #right-internal{
         display: flex;
         justify-content: space-between;
         align-items: center;
         padding: 10px 20px;
+        padding-right: 0px;
     }
     #right-external {
         margin-left: 80px;
     }
+    #body-content {
+        display: flex;
+        flex-direction: column;
+        width: var(--page-content-width);
+        padding: 10px 40px;
+    }
+    #body-outline {
+        display: flex;
+        justify-content: center;
+        height: 100%;
+    }
 </style>
 
-<header>
-    <nav>
-        <!-- Left side: Website favicon linking to home page -->
-        <a href="/">
-            <img src={ Favicon } alt="Website Favicon" style="width: 24px; height: 24px;">
-        </a>
-        <!-- Right side: internal & external links -->
-        <div class="right">
-            <!-- Links to internal pages -->
+<div id=outside>
+
+    <!-- Top pane of the website -->
+    <header>
+        <nav id=top>
+            <!-- Left side: Website favicon linking to home page -->
+            <a id=favicon href="/">
+                <img src={ Favicon } alt="Website Favicon" style="width: 50px; height: 50px;">
+            </a>
+            <!-- Right side: internal & external links -->
             <div id=right-internal>
-                <a href="/">profile</a>
-                <a href="/projects">projects</a>
-                <a href="/blog">blog</a>
+                <!-- Links to internal pages -->
+                <div id=right-internal>
+                    <a href="/">profile</a>
+                    <a href="/projects">projects</a>
+                    <a href="/blog">blog</a>
+                </div>
+                <!-- Links to external sites -->
+                <div id=right-external>
+                    <a href="https://github.com/ben-ruhlig">
+                        <img src={ GitIcon } alt="Github Favicon" style="width: 32px; height: 32px;">
+                    </a>
+                    <a href="https://www.linkedin.com/in/benruhlig/">
+                        <img src={ LinkedinIcon } alt="LinkedIn" style="width: 32px; height: 32px;">
+                    </a>
+                </div>
             </div>
-            <!-- Links to external sites -->
-            <div id=right-external>
-                <a href="https://github.com/ben-ruhlig">
-                    <img src={ GitIcon } alt="Github Favicon" style="width: 32px; height: 32px;">
-                </a>
-                <a href="https://www.linkedin.com/in/benruhlig/">
-                    <img src={ LinkedinIcon } alt="LinkedIn" style="width: 32px; height: 32px;">
-                </a>
+        </nav>
+    </header>
+
+    <!-- Middle content section of the website -->
+    <div id=body-outline>
+        <div id=body-content>
+            <div id=profile-picture>
+                <p>This is where the profile picture belongs</p>
+            </div>
+            <div id=portfolio-highlights>
+                <slot />
             </div>
         </div>
-    </nav>
-</header>
+    </div>
 
-<div id=container-body>
+    <!-- Bottom pane of the website -->
+    <footer>
+        <nav id=bottom>
+            <!-- Left side: Website favicon linking to home page -->
+            <a id=copyright href="/">&copy 2024 Benjamin Ruhlig</a>
+            <!-- Right side: internal & external links -->
+            <div class="right">
+                <!-- Links to internal pages -->
+                <div id=right-internal>
+                    <a href="/">profile</a>
+                    <a href="/projects">projects</a>
+                    <a href="/blog">blog</a>
+                </div>
+            </div>
+        </nav>
+    </footer>
 
 </div>
 
-<slot />
