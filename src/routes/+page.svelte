8b9b5ba7f4c47cc118a4fdfbfbf4912bba28ce5a
Ben Ruhlig <benjamin.ruhlig@gmail.com>
2024-03-01 14:49:59 -0600

    flesh out profile body outline

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,10 +16,30 @@
         <h1>Hey, I'm { data.firstName } { data.lastName }</h1>
         <img src={ profilePic } alt="Profile picture"/>
     </div>
-    <div>
-        <h1>Hey, I'm {data.first_name} {data.last_name}</h1>
-        <p>I'm a finance professional that's has since pursued my passion to learn computer science and build things.</p>
-    </div>
     <div id=portfolio-highlights>
+        <h2>Projects</h2>
+        <p>
+            This section should show my greatest hits projects (or latest 3), with a "see more..." link to all my projects. Probably tile format.
+        </p>
+        <p>
+            -------- Make this a fancy section dividing line ------------------------ 
+        </p>
     </div>
+    <div>
+        <h2>Projects</h2>
+        <h3>Summary</h3>
+        <p>
+            I'm a finance professional with 10 years <b>sTODO: Make experience # dynamic</b> of experience. I'm also pursing a Master's in Computer and Information Technology (MCIT)
+            <b>TODO: Turn into a link to the website</b> at the University of Pennsylvania (UPenn).
+        </p>
+        <h3> My Story </h3>
+        <p>
+            My love for technology has very practical beginnings. I stumbled into it trying to solve the numerous pain points I experienced as a finance professional in the modern era....
+        </p>
+        <h3> Generic Resume </h3>
+        <p>
+            Use LinkedIn API to automatically update from my LinkedIn profile.
+        </p>
+    </div>
+
 </div>
--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,7 +1,9 @@
 <h1>Blog</h1>
 
 <ul>
-    <li>Blog post #1</li>
-    <li>Blog post #2</li>
-    <li>Blog post #3</li>
+    <li>To spreadsheet or not to spreadsheet, that is the question.</li>
+    <li>My career evolution: From CPA to VBA</li>
+    <li>Why I blog: An excercise in self-reflection</li>
+    <li>What people get wrong applying technology to FP&A</li>
+    <li>My views on specialization: Nuance is crucial</li>
 </ul>
