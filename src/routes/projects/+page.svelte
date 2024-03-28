3c3a763a2ce4e3b75edc29c24451df0f20550108
ben-ruhlig <benjamin.ruhlig@gmail.com>
2024-03-28 13:02:12 +0000

    set default project for /projects page onMount

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,2 +1,9 @@
-<p>Select a project above</p>
-
+// src/routes/index.svelte
+<script>
+    import { onMount } from 'svelte';
+    import { goto } from '$app/navigation';
+    
+    onMount(() => {
+        goto('/projects/project1');
+    });
+</script>
